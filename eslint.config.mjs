027896924e5497819c3ee9f57f6b870f8ec656a8
Sean Kerwin <skerwin@mac.com>
2025-07-05 01:49:14 -0700

    Use mjs for eslint config.  Exclude it from tsconfig.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', 'eslint.config.js'],
+    ignores: ['dist/**', 'eslint.config.mjs'],
   },
   {
     rules: {
